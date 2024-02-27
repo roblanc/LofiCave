627265660d80daedbf5546d2f4238d6a9df2dd3d
netrebnic <70092629+roblanc@users.noreply.github.com>
2024-02-27 09:59:35 +0000

    privacy mode true
    update

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 var player;
 
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('youtube-player', {
-        videoId: 'rPjez8z61rI', // Replace with the ID of your video
-        playerVars: { 
-            'autoplay': 1, 
-            'controls': 0,  // Customize controls if needed
-            'loop': 1,      // Loop the video  
-            'playlist': 'rPjez8z61rI' // Loop a single video
-        },
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
+  player = new YT.Player('youtube-player', {
+    videoId: 'rPjez8z61rI', // Replace with the ID of your video
+    playerVars: { 
+      'autoplay': 1, 
+      'controls': 0,  // Customize controls if needed
+      'loop': 1,      // Loop the video  
+      'playlist': 'rPjez8z61rI', // Loop a single video
+      'privacyMode': true // Add privacyMode here
+    },
+    events: {
+      'onReady': onPlayerReady
+    }
+  });
 }
 
 function onPlayerReady(event) {
-    event.target.playVideo();
+  event.target.playVideo();
 }
