e05647c5ec85bd6a56e74fd4e2cde8e26ca03f4c
netrebnic <70092629+roblanc@users.noreply.github.com>
2024-02-27 10:14:05 +0000

    macro update
    update

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var player;
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('youtube-player', {
-    videoId: 'rPjez8z61rI', // Replace with the ID of your video
-    playerVars: { 
-      'autoplay': 1, 
-      'controls': 0,  // Customize controls if needed
-      'loop': 1,      // Loop the video  
-      'playlist': 'rPjez8z61rI', // Loop a single video
-      'privacyMode': true // Add privacyMode here
-    },
-    events: {
-      'onReady': onPlayerReady
-    }
-  });
+    player = new YT.Player('youtube-player', {
+        videoId: 'rPjez8z61rI', // Replace with the ID of your video
+        playerVars: {
+            'autoplay': 1,
+            'controls': 0,  // Customize controls if needed
+            'loop': 1,      // Loop the video  
+            'playlist': 'rPjez8z61rI', // Loop a single video
+            'modestbranding': 1, // Limit YouTube branding
+            'rel': 0, // Do not show related videos on playback end
+            'showinfo': 0 // Do not show video info
+        },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
 }
 
 function onPlayerReady(event) {
-  event.target.playVideo();
+    event.target.playVideo();
+}
+
+// Loop the video when it ends.
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        player.playVideo(); 
+    }
 }
